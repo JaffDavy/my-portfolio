920dd203bf2901f8a4310f8282354d1e9deec2cb
Jaffdavy <jaffdavy100@gmail.com>
2023-12-06 10:17:38 +0100

    my portfolio

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-const project = ["3-button-project", "cat-photos", "app-landing-page", "calculator", 
-"final-project", "technical-document", "free-code-camp", "The-newyork-times"," newtribute"," tribute-page"]
-let technologies = ["HTML", "CSS", "JAVASCRIPT", "c++"]
+const project = ['3-button-project', 'cat-photos', 'app-landing-page', 'calculator', 'final-project', 'technical-document', 'free-code-camp', 'The-newyork-times', 'newtribute', 'tribute-page']
+const technologies = ['HTML', 'CSS', 'JAVASCRIPT', 'c++']
 
 function updateProjectList () {
-const projectList = document.getElementById('projectsList')
-projectList.innerHTML = ''
+  const projectList = document.getElementById('projectsList')
+  projectList.innerHTML = ''
 
-project.forEach(project => {
+  project.forEach(project => {
     const li = document.createElement('li')
     li.textContent = project
     projectList.appendChild(li)
-})
+  })
 }
 
-function updateTechList() {
-const techList =document.getElementById('techList')
-techList.innerHTML = ''
+function updateTechList () {
+  const techList = document.getElementById('techList')
+  techList.innerHTML = ''
 
-technologies.forEach(tech => {
+  technologies.forEach(tech => {
     const li = document.createElement('li')
     li.textContent = tech
     techList.appendChild(li)
-})
+  })
 }
 
-function addTechnology() {
-const newTechInput =document.getElementById('newTech')
-const newTech = newTechInput.value.trim()
+function addTechnology () {
+  const newTechInput =document.getElementById('newTech')
+  const newTech = newTechInput.value.trim()
 
-if (newTech !== '') {
+  if (newTech !== '') {
     technologies.push(newTech)
     updateTechList()
     newTechInput.value = ''
-}
+  }
 }
 
 updateProjectList()
